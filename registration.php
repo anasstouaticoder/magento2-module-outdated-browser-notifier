cbacab592730c1674954b6ebd20e9f4730902f79
Anass Touati <anass.touati@hagergroup-external.com>
2024-05-20 18:20:53 +0200

    first release of the module OutdatedBrowserNotifier

---
+++ registration.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Copyright (c) 2024
+ * MIT License
+ * Module AnassTouatiCoder_OutdatedBrowserNotifier
+ * Author Anass TOUATI anass1touati@gmail.com
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'AnassTouatiCoder_OutdatedBrowserNotifier', __DIR__);
